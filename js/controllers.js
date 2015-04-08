7366120c225934299ac1f11d56e170b38c00bbc7
sohan1 <shn619@gmail.com>
2015-04-08 15:07:46 +0530

    changes
    changes

--- js/controllers.js
+++ js/controllers.js
@@ -1,40 +1,57 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
-//  // Form data for the login modal
-//  $scope.loginData = {};
-//
-//  // Create the login modal that we will use later
-//  $ionicModal.fromTemplateUrl('templates/login.html', {
-//    scope: $scope
-//  }).then(function(modal) {
-//    $scope.modal = modal;
-//  });
-//
-//  // Triggered in the login modal to close it
-//  $scope.closeLogin = function() {
-//    $scope.modal.hide();
-//  };
-//
-//  // Open the login modal
-//  $scope.login = function() {
-//    $scope.modal.show();
-//  };
-//
-//  // Perform the login action when the user submits the login form
-//  $scope.doLogin = function() {
-//    console.log('Doing login', $scope.loginData);
-//
-//    // Simulate a login delay. Remove this and replace with your login
-//    // code if using a login system
-//    $timeout(function() {
-//      $scope.closeLogin();
-//    }, 1000);
-//  };
+.controller('AppCtrl', function ($scope, $ionicModal, $timeout) {
+    //  // Form data for the login modal
+    //  $scope.loginData = {};
+    //
+    //  // Create the login modal that we will use later
+    //  $ionicModal.fromTemplateUrl('templates/login.html', {
+    //    scope: $scope
+    //  }).then(function(modal) {
+    //    $scope.modal = modal;
+    //  });
+    //
+    //  // Triggered in the login modal to close it
+    //  $scope.closeLogin = function() {
+    //    $scope.modal.hide();
+    //  };
+    //
+    //  // Open the login modal
+    //  $scope.login = function() {
+    //    $scope.modal.show();
+    //  };
+    //
+    //  // Perform the login action when the user submits the login form
+    //  $scope.doLogin = function() {
+    //    console.log('Doing login', $scope.loginData);
+    //
+    //    // Simulate a login delay. Remove this and replace with your login
+    //    // code if using a login system
+    //    $timeout(function() {
+    //      $scope.closeLogin();
+    //    }, 1000);
+    //  };
 })
 
-.controller('HomeCtrl', function($scope) {})
-.controller('SearchCtrl', function($scope) {})
-.controller('ProductCtrl', function($scope) {})
-.controller('PlaylistCtrl', function($scope, $stateParams) {
-});
+.controller('HomeCtrl', function ($scope) {})
+    .controller('SearchCtrl', function ($scope) {})
+    .controller('ProductCtrl', function ($scope) {
+
+        $ionicModal.fromTemplateUrl('templates/purchase.html', {
+            scope: $scope,
+            animation: 'slide-in-up'
+        }).then(function (modal) {
+            $scope.modal = modal;
+        });
+
+        $scope.openedit = function () {
+            $scope.modal.show();
+        }
+
+        $scope.closeModal = function () {
+            $scope.modal.hide();
+        };
+
+
+    })
+    .controller('PlaylistCtrl', function ($scope, $stateParams) {});
