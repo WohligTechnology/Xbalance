44a84c8b3bce5054598977a66a58d43aa8fdf715
sohan1 <shn619@gmail.com>
2015-04-15 11:36:11 +0530

    Changes
    Changes

--- js/controllers.js
+++ js/controllers.js
@@ -68,97 +68,113 @@ angular.module('starter.controllers', [])
 .controller('AboutCtrl', function ($scope, $stateParams) {})
 
 .controller('ProfileCtrl', function ($scope, $stateParams, $ionicModal, $ionicSlideBoxDelegate) {
-    $scope.aImages = [{
-        'src': 'http://lorempixel.com/g/100/100/?1',
-        'srclg': 'http://lorempixel.com/g/100/100/?1',
-        'msg': 'Swipe me to the left. Tap/click to close'
+
+
+        $scope.aImages = [{
+            'src': 'http://ionicframework.com/img/ionic-logo-blog.png',
+            //            'msg': 'Swipe me to the left. Tap/click to close'
      }, {
-        'src': 'http://lorempixel.com/g/100/100/?2',
-        'srclg': 'http://lorempixel.com/g/100/100/?2',
-        'msg': 'Title Two'
+            'src': 'http://ionicframework.com/img/ionic_logo.svg',
+            //            'msg': ''
       }, {
-        'src': 'http://lorempixel.com/g/100/100/?3',
-        'srclg': 'http://lorempixel.com/g/100/100/?3',
-        'msg': 'Title Three'
-      }, {
-        'src': 'http://lorempixel.com/g/100/100/?4',
-        'srclg': 'http://lorempixel.com/g/100/100/?4',
-        'msg': 'Title Four'
-      }, {
-        'src': 'http://lorempixel.com/g/100/100/?5',
-        'srclg': 'http://lorempixel.com/g/100/100/?5',
-        'msg': 'Title Two'
-      }, {
-        'src': 'http://lorempixel.com/g/100/100/?6',
-        'srclg': 'http://lorempixel.com/g/100/100/?6',
-        'msg': 'Title Three'
+            'src': 'http://ionicframework.com/img/homepage/phones-weather-demo@2x.png',
+            //            'msg': ''
     }];
 
-    $ionicModal.fromTemplateUrl('templates/image-modal.html', {
-        scope: $scope,
-        animation: 'slide-in-up'
-    }).then(function (modal) {
-        $scope.modal = modal;
+        //        $ionicModal.fromTemplateUrl('templates/image-modal.html', {
+        //            scope: $scope,
+        //            animation: 'slide-in-up'
+        //        }).then(function (modal) {
+        //            $scope.modal = modal;
+        //        });
+        //
+        //        $scope.openModal = function () {
+        //            $ionicSlideBoxDelegate.slide(0);
+        //            $scope.modal.show();
+        //        };
+        //
+        //        $scope.closeModal = function () {
+        //            $scope.modal.hide();
+        //        };
+        //
+        //        // Cleanup the modal when we're done with it!
+        //        $scope.$on('$destroy', function () {
+        //            $scope.modal.remove();
+        //        });
+        //        // Execute action on hide modal
+        //        $scope.$on('modal.hide', function () {
+        //            // Execute action
+        //        });
+        //        // Execute action on remove modal
+        //        $scope.$on('modal.removed', function () {
+        //            // Execute action
+        //        });
+        //        $scope.$on('modal.shown', function () {
+        //            console.log('Modal is shown!');
+        //        });
+        //
+        //        // Call this functions if you need to manually control the slides
+        //        $scope.next = function () {
+        //            $ionicSlideBoxDelegate.next();
+        //        };
+        //
+        //        $scope.previous = function () {
+        //            $ionicSlideBoxDelegate.previous();
+        //        };
+        //
+        //        $scope.goToSlide = function (index) {
+        //            $scope.modal.show();
+        //            $ionicSlideBoxDelegate.slide(index);
+        //        }
+        //
+        //        // Called each time the slide changes
+        //        $scope.slideChanged = function (index) {
+        //            $scope.slideIndex = index;
+        //        };
+        //                $ionicModal.fromTemplateUrl('templates/resetpswd.html', {
+        $ionicModal.fromTemplateUrl('templates/image-modal.html', {
+            scope: $scope,
+            animation: 'slide-in-up'
+        }).then(function (modal) {
+            $scope.modal = modal;
+        });
+
+        $scope.openedit = function () {
+            $scope.modal.show();
+        };
+
+        $scope.closeModal = function () {
+            $scope.modal.hide();
+        }; 
+//    $ionicModal.fromTemplateUrl('templates/resetpswd.html', {
+//            scope: $scope,
+//            animation: 'slide-in-up'
+//        }).then(function (modal) {
+//            $scope.modal = modal;
+//        });
+//
+//        $scope.openpswd = function () {
+//            $scope.modal.show();
+//        };
+//
+//        $scope.closeModal = function () {
+//            $scope.modal.hide();
+//        };
+    })
+    .controller('YourBalCtrl', function ($scope, $stateParams, $ionicModal) {
+        $ionicModal.fromTemplateUrl('templates/addbalance.html', {
+            scope: $scope,
+            animation: 'slide-in-up'
+        }).then(function (modal) {
+            $scope.modal = modal;
+        });
+
+        $scope.openedit = function () {
+            $scope.modal.show();
+        }
+
+        $scope.closeModal = function () {
+            $scope.modal.hide();
+        };
+
     });
-
-    $scope.openModal = function () {
-        $scope.modal.show();
-        // Important: This line is needed to update the current ion-slide's width
-        // Try commenting this line, click the button and see what happens
-        $ionicSlideBoxDelegate.update();
-    };
-
-    $scope.closeModal = function () {
-        $scope.modal.hide();
-    };
-
-    // Cleanup the modal when we're done with it!
-    $scope.$on('$destroy', function () {
-        $scope.modal.remove();
-    });
-    // Execute action on hide modal
-    $scope.$on('modal.hide', function () {
-        // Execute action
-    });
-    // Execute action on remove modal
-    $scope.$on('modal.removed', function () {
-        // Execute action
-    });
-    $scope.$on('modal.shown', function () {
-        console.log('Modal is shown!');
-    });
-
-    // Call this functions if you need to manually control the slides
-    $scope.next = function () {
-        $ionicSlideBoxDelegate.next();
-    };
-
-    $scope.previous = function () {
-        $ionicSlideBoxDelegate.previous();
-    };
-
-    // Called each time the slide changes
-    $scope.slideChanged = function (index) {
-        $scope.slideIndex = index;
-    };
-
-
-})
-
-.controller('YourBalCtrl', function ($scope, $stateParams, $ionicModal) {
-    $ionicModal.fromTemplateUrl('templates/addbalance.html', {
-        scope: $scope,
-        animation: 'slide-in-up'
-    }).then(function (modal) {
-        $scope.modal = modal;
-    });
-
-    $scope.openedit = function () {
-        $scope.modal.show();
-    }
-
-    $scope.closeModal = function () {
-        $scope.modal.hide();
-    };
-
-});
