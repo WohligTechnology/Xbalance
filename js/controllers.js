a5b2dcc28fc104c6bb2fd91340509b8a01727333
Chintan Shah <chintan@wohlig.com>
2015-05-21 16:05:07 +0530

    changes

--- js/controllers.js
+++ js/controllers.js
@@ -460,7 +460,13 @@ angular.module('starter.controllers', ['myservices','ngCordova'])
     });
 
     $scope.changeprofileimage = function () {
-        console.log("ABCD");
+        console.log("take picture");
+        var options = {
+            quality: 40,
+            destinationType: Camera.DestinationType.NATIVE_URI,
+            sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
+            encodingType: Camera.EncodingType.JPEG
+        };
         $cordovaCamera.getPicture(options).then(function (imageData) {
             // Success! Image data is here
             console.log("here in upload image");
