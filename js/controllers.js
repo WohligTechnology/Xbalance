94ca478142bba8f417f9f6e0750da19c787ddca2
Pooja Thakare <pthakare33@gmail.com>
2015-06-27 14:53:41 +0530

    uncomment options

--- js/controllers.js
+++ js/controllers.js
@@ -965,14 +965,14 @@ angular.module('starter.controllers', ['myservices', 'ngCordova'])
 	$scope.showloading();
 
 	//Hide when on PC
-//		var options = {
-//			quality: 20,
-//			destinationType: Camera.DestinationType.FILE_URI,
-//			sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
-//			allowEdit: true,
-//			encodingType: Camera.EncodingType.JPEG,
-//			saveToPhotoAlbum: true
-//		};
+		var options = {
+			quality: 20,
+			destinationType: Camera.DestinationType.FILE_URI,
+			sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
+			allowEdit: true,
+			encodingType: Camera.EncodingType.JPEG,
+			saveToPhotoAlbum: true
+		};
 	//    var options = {
 	//            maximumImagesCount: 1,
 	//            width: 800,
@@ -1746,14 +1746,14 @@ angular.module('starter.controllers', ['myservices', 'ngCordova'])
 		}
 		//edit products and status end
 		//Hide when on PC
-//			var options = {
-//				quality: 20,
-//				destinationType: Camera.DestinationType.FILE_URI,
-//				sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
-//				allowEdit: true,
-//				encodingType: Camera.EncodingType.JPEG,
-//				saveToPhotoAlbum: true
-//			};
+			var options = {
+				quality: 20,
+				destinationType: Camera.DestinationType.FILE_URI,
+				sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
+				allowEdit: true,
+				encodingType: Camera.EncodingType.JPEG,
+				saveToPhotoAlbum: true
+			};
 	
 	
 		//upload editproductimage start
